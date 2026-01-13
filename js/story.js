d1ee83e632f3bb972f673a35ba5951e826fda1e4
gilles colling <gilles.colling051@gmail.com>
2026-01-13 15:27:14 +0100

    Fix: Use Tobi as background instead of character sprite

--- js/story.js
+++ js/story.js
@@ -931,10 +931,7 @@ const story = {
   },
   "dnd_group_tobi": {
     "id": "dnd_group_tobi",
-    "bg": "staircase_downstairs.png",
-    "chars": [
-      "tobi_shadowrun.png"
-    ],
+    "bg": "../char/tobi_shadowrun.png",
     "textBlocks": [
       "As you head up the stairs, you nearly collide with Tobi coming down. He's clutching a massive Shadowrun rulebook. \"Andi! Good timing. I'm about to settle something with those D&D peasants.\" He grins. \"I'm challenging them to summarize the Shadowrun rules on one A4 page.\"",
       "You know this is impossible. You once tried it yourself. The matrix rules alone would fill three pages. You wish him luck and escape before he can explain the dice pool system to you."
---
+++ scenes/dnd_group_tobi.md
@@ -0,0 +1,14 @@
+---
+id: dnd_group_tobi
+bg: ../char/tobi_shadowrun.png
+---
+
+As you head up the stairs, you nearly collide with Tobi coming down. He's clutching a massive Shadowrun rulebook. "Andi! Good timing. I'm about to settle something with those D&D peasants." He grins. "I'm challenging them to summarize the Shadowrun rules on one A4 page."
+
+---
+
+You know this is impossible. You once tried it yourself. The matrix rules alone would fill three pages. You wish him luck and escape before he can explain the dice pool system to you.
+
+### Choices
+
+- Continue upstairs → stairs_0
